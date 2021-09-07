8b760b09560c333c01b3a305eae8b63ce70ca8fc
ethoque <abidsemail1@gmail.com>
2021-09-07 07:39:46 -0700

    Added support for rooms
    Multiple games can now happen at the same time.

--- server.js
+++ server.js
@@ -2,58 +2,66 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 
 const server = http.createServer(app)
 const io = socketio(server)
-const list = []
-var moves = []
+var list = []
+var map = new Map();
 
 
 io.on('connection', (socket) => {
-    list.push(socket.id)
+    const { roomId } = socket.handshake.query;
+    var room = map.get(roomId);
+    
+    if(room === undefined){
+        map.set(roomId, [[],[]]); // 0th array moves, 1st total users
+    }
+    socket.join(roomId)
+    map.get(roomId)[1].push(socket.id)
     console.log(socket.id + " has joined")
-    console.log(list.length + " total clients")
+    console.log('room: '+ roomId +' has ' +map.get(roomId)[1].length + " total clients")
 
-    if(list.length == 2 ){
-        io.emit("restart")
-        io.to(list[0]).emit("playerOne")
+    if(map.get(roomId)[1].length == 2 ){
+        io.to(roomId).emit("restart")
+        io.to(map.get(roomId)[1][0]).emit("playerOne")
         socket.emit("changePlayer")
     }
-    if(list.length > 2){
+    if(map.get(roomId)[1].length > 2){
         socket.emit("disable")
-        for(let i = 0; i<moves.length; i++){
-            socket.emit("moveEvent",moves[i])
+        for(let i = 0; i<map.get(roomId)[0].length; i++){
+            socket.emit("moveEvent",map.get(roomId)[0][i])
         }
     }
     socket.on("moveEvent" , (data) => {
-        moves.push(data)
-        socket.broadcast.emit("moveEvent",data);
+        map.get(roomId)[0].push(data)
+        socket.to(roomId).emit("moveEvent",data);
     })
 
     socket.on("disconnect", () => {
-        for(let i = 0; i<list.length; i++){
-            if(socket.id === list[i]){
-                list.splice(i,1);
+        for(let i = 0; i<map.get(roomId)[1].length; i++){
+            if(socket.id === map.get(roomId)[1][i]){
+                map.get(roomId)[1].splice(i,1);
+                socket.leave(roomId)
                 if(i<2){
-                    io.emit("restart")
-                    moves = []
-                    if(list.length>1){
-                        io.to(list[0]).emit("enable")
-                        io.to(list[0]).emit("playerOne")
-                        io.to(list[1]).emit("enable")
-                        io.to(list[1]).emit("changePlayer")
+                    io.in(roomId).emit("restart")
+                    map.get(roomId)[0] = []
+                    if(map.get(roomId)[1].length>1){
+                        io.to(map.get(roomId)[1][0]).emit("enable")
+                        io.to(map.get(roomId)[1][0]).emit("playerOne")
+                        io.to(map.get(roomId)[1][1]).emit("enable")
+                        io.to(map.get(roomId)[1][1]).emit("changePlayer")
                     }
                     else if(list.length==1){
-                        io.to(list[0]).emit("enable")
+                        io.to(map.get(roomId)[1][0]).emit("enable")
                     }
-                }      
+                } 
                 break;
             }
         }
         console.log(socket.id + " has left")
-        console.log(list.length + " total clients")
+        console.log(map.get(roomId)[1].length + " total clients")
     });
 })
 
